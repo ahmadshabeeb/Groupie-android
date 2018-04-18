7120981751f006583a80ec6d95e799984f602a82
Matay Marani <mataymarani@n163-p98.eduroam.kth.se>
2018-04-18 16:48:37 +0200

    Registration basic Implementation

--- app/src/main/java/app/kth/com/groupie/MainActivity.java
+++ app/src/main/java/app/kth/com/groupie/MainActivity.java
@@ -1,7 +1,10 @@
 package app.kth.com.groupie;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import app.kth.com.groupie.registration.RegistrationActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
     }
 }

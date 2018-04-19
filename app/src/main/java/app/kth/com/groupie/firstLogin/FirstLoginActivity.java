abd6273b3e2fa1a2c7443317baecaf4e461db5fe
Matay Mayrany <mayrany@kth.se>
2018-04-19 14:19:54 +0200

    started working on first login page

--- app/src/main/java/app/kth/com/groupie/firstLogin/FirstLoginActivity.java
+++ app/src/main/java/app/kth/com/groupie/firstLogin/FirstLoginActivity.java
@@ -2,12 +2,30 @@ package app.kth.com.groupie.firstLogin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import app.kth.com.groupie.Data.Structure.PrivateProfile;
+import app.kth.com.groupie.R;
+
 
 public class FirstLoginActivity extends AppCompatActivity {
 
+
+    PrivateProfile privateProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_login);
+
+        privateProfile = new PrivateProfile();
+
+    }
+
+
+    public void addInfoToProfile(String firstName, String lastName, String school, String fieldOfStudy, View v){
+        //ADD INFO TO THE CREATED PRIVATE PROFILE
+        EditText
+
     }
 }
---
+++ app/src/main/java/app/kth/com/groupie/firstLogin/UserProfileFragment.java
@@ -0,0 +1,103 @@
+package app.kth.com.groupie.firstLogin;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import app.kth.com.groupie.R;
+
+public class UserProfileFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    private String [] domains = {"kth.se", "su.se", "ki.se", "hhs.se",
+            "sh.se", "kmh.se", "shh.se", "fhs.se", "esh.se", "konstfack.se",
+            "smi.se", "uniarts.se", "kkh.se", "ths.se", "chiro-student.se", "gih.se", "rkh.se"};
+    private String [] nameOfDomains = {"Kungliga Tekniska Högskolan", "Stockholms Universitet", "Karolinska Institutet",
+            "Handelshögskolan", "Södertörn Högskola", "Kungliga Musikhögskolan", "Sophiahemmet Högskola",
+            "Försvarshögskolan", "Ersta Sköndal Högskola", "Konstfack", "Stockholms Musikpedagogiska Högskola",
+            "Stockholms Konstnärliga Högskola", "Kungliga Konsthögskolan", "Teologiska Högskolan Stockholm",
+            "Skandinaviska Kiropraktorhögskolan", "Gymnastik- och idrottshögskolan", "Röda Korsets Högskolaf"};
+
+    FirebaseAuth mAuth;
+    EditText firstName;
+    EditText lastName;
+    EditText fieldOfStudy;
+    EditText school;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ViewGroup rootview = (ViewGroup) inflater.inflate(R.layout.fragment_user_profile, container, false);
+        mAuth = FirebaseAuth.getInstance();
+
+        //INPUT EDITTEXT FIELDS
+        firstName = (EditText) rootview.findViewById(R.id.firstname_edittext);
+        lastName = (EditText) rootview.findViewById(R.id.lastname_edittext);
+        fieldOfStudy = (EditText) rootview.findViewById(R.id.fieldofstudy_edittext);
+        school = (EditText) rootview.findViewById(R.id.school_edittext);
+        school.setText(getNameOfSchool());
+
+
+        Button nextButton = (Button) rootview.findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addInfoAndMoveOn(v);
+            }
+        });
+
+        return rootview;
+    }
+
+    private void addInfoAndMoveOn(View v){
+        String firstNameInput = firstName.getText().toString();
+        String lastNameInput = lastName.getText().toString();
+        String schoolInput = school.getText().toString();
+        String fieldOfStudyInput = fieldOfStudy.getText().toString();
+
+
+        FirstLoginActivity activity = (FirstLoginActivity) getActivity();
+        activity.addInfoToProfile(firstNameInput, lastNameInput, schoolInput, fieldOfStudyInput, v);
+
+    }
+
+    private String getNameOfSchool(){
+        String email = mAuth.getCurrentUser().getEmail();
+        String [] split = email.split("@");
+        String domain = split[1];
+        for (int index = 0; index < domains.length; index++){
+            if(domain.equals(domains[index])){
+                return nameOfDomains[index];
+            }
+        }
+        return "";
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+    }
+
+
+}

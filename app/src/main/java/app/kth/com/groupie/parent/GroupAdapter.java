f2aae0039e63b51fa82af627bdd67572d148965d
5habeeb <ahmadshabeeb93@gmail.com>
2018-04-23 17:30:36 +0200

    updating the implementation of cardview for group list

--- app/src/main/java/app/kth/com/groupie/parent/GroupAdapter.java
+++ app/src/main/java/app/kth/com/groupie/parent/GroupAdapter.java
@@ -2,7 +2,6 @@ package app.kth.com.groupie.parent;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
